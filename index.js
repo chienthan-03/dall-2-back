9916492de86e3437fdb8b0b75098b7366009dff5
LuanThanh10d <khucthanhluan1002@gmail.com>
2023-05-14 22:04:02 +0700

    two commit

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://dalle2-seven.vercel.app"
+  );
+  next();
+});
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
